0009dd8210942ce396cec7de6a75636654453714
Deepu S <ss.deepu@gmail.com>
2025-08-25 22:48:30 +0000

    When click on Edit button of Cancellation Details, pop-up window will co

--- src/app/dashboard/agency-registration/page.tsx
+++ src/app/dashboard/agency-registration/page.tsx
@@ -813,7 +813,9 @@ export default function AgencyRegistrationPage() {
                     <div className="grid grid-cols-4 items-center gap-4">
                         <Label className="text-right">Date of Cancellation</Label>
                         <Popover>
-                        <PopoverTrigger asChild><Button variant="outline" className="col-span-3"><CalendarIcon className="mr-2 h-4 w-4"/>{cancellationData?.date ? format(cancellationData.date, 'dd/MM/yyyy') : 'Select'}</Button></PopoverTrigger>
+                        <PopoverTrigger asChild><Button variant="outline" className="col-span-3"><CalendarIcon className="mr-2 h-4 w-4"/>
+                            {cancellationData?.date ? format(cancellationData.date, 'dd/MM/yyyy') : 'Select'}
+                        </Button></PopoverTrigger>
                         <PopoverContent><Calendar mode="single" selected={cancellationData?.date} onSelect={(date) => setCancellationData(d => ({ ...d!, date: date }))} /></PopoverContent>
                         </Popover>
                     </div>
@@ -892,3 +894,5 @@ export default function AgencyRegistrationPage() {
     </>
   );
 }
+
+    
