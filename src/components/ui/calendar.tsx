d655fac45899cc4edb0e4892c5cc49fbdfcfbbbe
Deepu S <ss.deepu@gmail.com>
2025-09-03 20:07:29 +0000

    no change

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -7,12 +7,17 @@ import { DayPicker } from "react-day-picker"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = React.ComponentProps<typeof DayPicker> & {
+    fromYear?: number;
+    toYear?: number;
+};
 
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  fromYear = new Date().getFullYear() - 100,
+  toYear = new Date().getFullYear(),
   ...props
 }: CalendarProps) {
   return (
@@ -23,7 +28,9 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
+        caption_dropdowns: "flex gap-2",
+        vhidden: "hidden",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
