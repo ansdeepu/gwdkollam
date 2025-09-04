c58e2128e09a088a73209251f1bc0a99c7fd019b
Deepu S <ss.deepu@gmail.com>
2025-09-04 03:52:40 +0000

    I do not need this design. I need only The calendar design I previously

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -24,6 +24,7 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
+        caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -58,6 +59,9 @@ function Calendar({
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
+      captionLayout="dropdown-buttons"
+      fromYear={2010}
+      toYear={2050}
     />
   )
 }
