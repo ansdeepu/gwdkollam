aaf2c3242f9a39582f1adb6bf02f6d9603316702
Deepu S <ss.deepu@gmail.com>
2025-09-04 12:18:14 +0000

    npm run build
    > nextn@0.1.0 build
    > next build
    
      ▲ Next.js 14.2.4
      -

--- src/components/establishment/StaffForm.tsx
+++ src/components/establishment/StaffForm.tsx
@@ -1,9 +1,8 @@
-
 // src/components/establishment/StaffForm.tsx
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, FormProvider } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -13,6 +12,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormProvider,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
