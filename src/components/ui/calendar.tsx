00565e99d58a9cdb02a1bc98a4e5e7133562c315
Deepu S <ss.deepu@gmail.com>
2025-09-04 10:40:05 +0000

    The calendar currently displays only a single month. This is i need

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -14,7 +13,6 @@ function Calendar({
   className,
   classNames,
   showOutsideDays = true,
-  numberOfMonths = 2,
   ...props
 }: CalendarProps) {
   return (
@@ -26,7 +24,6 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
-        caption_dropdowns: "flex justify-center gap-1",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -58,7 +55,6 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
-      numberOfMonths={numberOfMonths}
       {...props}
     />
   )
@@ -66,4 +62,3 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
-
