fcce811107c03f47a0fc9a57d8fbf0d0cc9e0a35
Deepu S <ss.deepu@gmail.com>
2025-09-06 09:30:34 +0000

    In Dashboard, provide equal width and height to File Status Overview, Im

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -199,19 +199,19 @@ export default function DashboardPage() {
 
   return (
     <div className="space-y-6">
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-        <div className="lg:col-span-2 space-y-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-1">
           <FileStatusOverview 
             nonArsEntries={dashboardData.nonArsEntries}
             onOpenDialog={handleOpenDialog}
           />
         </div>
-         <div className="lg:col-span-1 space-y-6">
+        <div className="lg:col-span-1">
           <ImportantUpdates
             allFileEntries={dashboardData.allFileEntries}
           />
         </div>
-        <div className="lg:col-span-2 space-y-6">
+        <div className="lg:col-span-1">
           <NoticeBoard 
             staffMembers={dashboardData.staffMembers}
           />
