ccd6a629358229184fa5c985f43099c4d205a42f
Deepu S <ss.deepu@gmail.com>
2025-09-08 00:58:20 +0000

    Try fixing this error: `Server Error: Error: Cannot find module './948.j

--- next.config.js
+++ next.config.js
@@ -50,24 +50,14 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
-    
+  webpack: (config) => {
     // Fix for handlebars dependency issue with Genkit
     config.resolve.alias = {
       ...config.resolve.alias,
       'handlebars': require.resolve('handlebars/dist/handlebars.js'),
     };
-    
-    if (isServer) {
-        config.externals.push('@opentelemetry/instrumentation');
-    }
-
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
