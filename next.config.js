1623795cf27a946ffe1f91a0e419233738f2a487
Deepu S <ss.deepu@gmail.com>
2025-09-13 11:31:38 +0000

    npm run build
    > nextn@0.1.0 build
    > next build
    
     ⚠ Invalid next.config.

--- next.config.js
+++ next.config.js
@@ -50,13 +50,7 @@ const nextConfig = {
       },
     ],
   },
-  allowedDevOrigins: [
-      "*.google.com",
-      "*.cloud.google.com",
-      "*.firebase.google.com",
-      "*.cloud.goog",
-      "*.corp.google.com",
-  ],
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
