f67ca89304ca807a98451307965e11dea850c80e
Deepu S <ss.deepu@gmail.com>
2025-09-18 18:04:30 +0000

    no change

--- src/app/dashboard/agency-registration/page.tsx
+++ src/app/dashboard/agency-registration/page.tsx
@@ -1009,11 +1009,17 @@ export default function AgencyRegistrationPage() {
                                      <div className="space-y-2">
                                         <h4 className="font-medium">Owner Details</h4>
                                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-2 border rounded-md">
-                                          <FormField name="owner.name" render={({ field }) => <FormItem className="md:col-span-2"><FormLabel>Name &amp; Address of Owner</FormLabel><FormControl><Textarea {...field} value={field.value ?? ""} readOnly={isReadOnlyForForm} /></FormControl><FormMessage /></FormItem>} />
-                                          <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-4">
-                                            <FormField name="owner.mobile" render={({ field }) => <FormItem><FormLabel>Mobile No.</FormLabel><FormControl><Input {...field} value={field.value ?? ""} readOnly={isReadOnlyForForm} /></FormControl><FormMessage /></FormItem>} />
-                                            <FormField name="owner.secondaryMobile" render={({ field }) => <FormItem><FormLabel>Secondary Mobile No.</FormLabel><FormControl><Input {...field} value={field.value ?? ""} readOnly={isReadOnlyForForm} /></FormControl><FormMessage /></FormItem>} />
-                                          </div>
+                                          <FormItem className="md:col-span-2">
+                                            <FormField name="owner.name" render={({ field }) => (
+                                              <>
+                                                <FormLabel>Name &amp; Address of Owner</FormLabel>
+                                                <FormControl><Textarea {...field} value={field.value ?? ""} readOnly={isReadOnlyForForm} /></FormControl>
+                                                <FormMessage />
+                                              </>
+                                            )} />
+                                          </FormItem>
+                                          <FormField name="owner.mobile" render={({ field }) => <FormItem><FormLabel>Mobile No.</FormLabel><FormControl><Input {...field} value={field.value ?? ""} readOnly={isReadOnlyForForm} /></FormControl><FormMessage /></FormItem>} />
+                                          <FormField name="owner.secondaryMobile" render={({ field }) => <FormItem><FormLabel>Secondary Mobile No.</FormLabel><FormControl><Input {...field} value={field.value ?? ""} readOnly={isReadOnlyForForm} /></FormControl><FormMessage /></FormItem>} />
                                         </div>
                                     </div>
                                     <div className="space-y-2">
