f3be314bd1f37827dd22c8e6ada6b593d92c055e
Deepu S <ss.deepu@gmail.com>
2025-12-02 00:55:17 +0000

    In Establishment page, Remove edit icon by eye icon. eye icon represent

--- src/components/establishment/StaffTable.tsx
+++ src/components/establishment/StaffTable.tsx
@@ -150,7 +150,7 @@ export default function StaffTable({
                         <Tooltip>
                           <TooltipTrigger asChild>
                             <Button variant="ghost" size="icon" onClick={() => onEdit && onEdit(staff)} disabled={isViewer} className="h-8 w-8">
-                               {isViewer ? <Eye className="h-4 w-4" /> : <Edit3 className="h-4 w-4" />}
+                               <Eye className="h-4 w-4" />
                             </Button>
                           </TooltipTrigger>
                           <TooltipContent><p>{isViewer ? "View Details" : "Edit Staff"}</p></TooltipContent>
